41224220fb9b814179ee22d6523d93be102b8eac
dingyi.chen <dingyi.chen@innopeaktech.com>
2022-05-12 16:24:03 -0700

    Record an ARCore session to an MP4 file

--- samples/hello_ar_java/app/src/main/java/com/google/ar/core/examples/java/helloar/HelloArActivity.java
+++ samples/hello_ar_java/app/src/main/java/com/google/ar/core/examples/java/helloar/HelloArActivity.java
@@ -16,22 +16,33 @@
 
 package com.google.ar.core.examples.java.helloar;
 
+import android.Manifest;
+import android.content.ContentResolver;
+import android.content.ContentValues;
 import android.content.DialogInterface;
+import android.content.pm.PackageManager;
 import android.content.res.Resources;
 import android.media.Image;
+import android.net.Uri;
 import android.opengl.GLES30;
 import android.opengl.GLSurfaceView;
 import android.opengl.Matrix;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.PopupMenu;
 import android.widget.Toast;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import com.google.ar.core.Anchor;
 import com.google.ar.core.ArCoreApk;
 import com.google.ar.core.Camera;
@@ -46,6 +57,8 @@ import com.google.ar.core.Plane;
 import com.google.ar.core.Point;
 import com.google.ar.core.Point.OrientationMode;
 import com.google.ar.core.PointCloud;
+import com.google.ar.core.RecordingConfig;
+import com.google.ar.core.RecordingStatus;
 import com.google.ar.core.Session;
 import com.google.ar.core.Trackable;
 import com.google.ar.core.TrackingFailureReason;
@@ -70,15 +83,19 @@ import com.google.ar.core.examples.java.common.samplerender.arcore.PlaneRenderer
 import com.google.ar.core.examples.java.common.samplerender.arcore.SpecularCubemapFilter;
 import com.google.ar.core.exceptions.CameraNotAvailableException;
 import com.google.ar.core.exceptions.NotYetAvailableException;
+import com.google.ar.core.exceptions.RecordingFailedException;
 import com.google.ar.core.exceptions.UnavailableApkTooOldException;
 import com.google.ar.core.exceptions.UnavailableArcoreNotInstalledException;
 import com.google.ar.core.exceptions.UnavailableDeviceNotCompatibleException;
 import com.google.ar.core.exceptions.UnavailableSdkTooOldException;
 import com.google.ar.core.exceptions.UnavailableUserDeclinedInstallationException;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.ByteBuffer;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 
@@ -93,6 +110,17 @@ public class HelloArActivity extends AppCompatActivity implements SampleRender.R
 
   private static final String SEARCHING_PLANE_MESSAGE = "Searching for surfaces...";
   private static final String WAITING_FOR_TAP_MESSAGE = "Tap on a surface to place an object.";
+  private final String MP4_VIDEO_MIME_TYPE = "video/mp4";
+  private final int REQUEST_WRITE_EXTERNAL_STORAGE = 1;
+
+  // Represents the app's working state.
+  public enum AppState {
+    Idle,
+    Recording
+  }
+
+  // Tracks app's specific state changes.
+  private AppState appState = AppState.Idle;
 
   // See the definition of updateSphericalHarmonicsCoefficients for an explanation of these
   // constants.
@@ -659,6 +687,186 @@ public class HelloArActivity extends AppCompatActivity implements SampleRender.R
         }
       }
     }
+  }
+
+  // Handle the "Record" button click event.
+  public void onClickRecord(View view) {
+    Log.d(TAG, "onClickRecord");
+
+    // Check the app's internal state and switch to the new state if needed.
+    switch (appState) {
+      // If the app is not recording, begin recording.
+      case Idle: {
+        boolean hasStarted = startRecording();
+        Log.d(TAG, String.format("onClickRecord start: hasStarted %b", hasStarted));
+
+        if (hasStarted)
+          appState = AppState.Recording;
+
+        break;
+      }
+
+      // If the app is recording, stop recording.
+      case Recording: {
+        boolean hasStopped = stopRecording();
+        Log.d(TAG, String.format("onClickRecord stop: hasStopped %b", hasStopped));
+
+        if (hasStopped)
+          appState = AppState.Idle;
+
+        break;
+      }
+
+      default:
+        // Do nothing.
+        break;
+    }
+
+    updateRecordButton();
+  }
+
+  // Update the "Record" button based on app's internal state.
+  private void updateRecordButton() {
+    View buttonView = findViewById(R.id.record_button);
+    Button button = (Button) buttonView;
+
+    switch (appState) {
+      case Idle:
+        button.setText("Record");
+        break;
+      case Recording:
+        button.setText("Stop");
+        break;
+    }
+  }
+
+  private boolean startRecording() {
+    Uri mp4FileUri = createMp4File();
+    if (mp4FileUri == null)
+      return false;
+
+    Log.d(TAG, "startRecording at: " + mp4FileUri);
+
+    pauseARCoreSession();
+
+    // Configure the ARCore session to start recording.
+    RecordingConfig recordingConfig = new RecordingConfig(session)
+        .setMp4DatasetUri(mp4FileUri)
+        .setAutoStopOnPause(true);
+
+    try {
+      // Prepare the session for recording, but do not start recording yet.
+      session.startRecording(recordingConfig);
+    } catch (RecordingFailedException e) {
+      Log.e(TAG, "startRecording - Failed to prepare to start recording", e);
+      return false;
+    }
+
+    boolean canResume = resumeARCoreSession();
+    if (!canResume)
+      return false;
+
+    // Correctness checking: check the ARCore session's RecordingState.
+    RecordingStatus recordingStatus = session.getRecordingStatus();
+    Log.d(TAG, String.format("startRecording - recordingStatus %s", recordingStatus));
+    return recordingStatus == RecordingStatus.OK;
+  }
+
+  private boolean stopRecording() {
+    try {
+      session.stopRecording();
+    } catch (RecordingFailedException e) {
+      Log.e(TAG, "stopRecording - Failed to stop recording", e);
+      return false;
+    }
+
+    // Correctness checking: check if the session stopped recording.
+    return session.getRecordingStatus() == RecordingStatus.NONE;
+  }
+
+  private void pauseARCoreSession() {
+    // Pause the GLSurfaceView so that it doesn't update the ARCore session.
+    // Pause the ARCore session so that we can update its configuration.
+    // If the GLSurfaceView is not paused,
+    //   onDrawFrame() will try to update the ARCore session
+    //   while it's paused, resulting in a crash.
+    surfaceView.onPause();
+    session.pause();
+  }
+
+  private boolean resumeARCoreSession() {
+    // We must resume the ARCore session before the GLSurfaceView.
+    // Otherwise, the GLSurfaceView will try to update the ARCore session.
+    try {
+      session.resume();
+    } catch (CameraNotAvailableException e) {
+      Log.e(TAG, "CameraNotAvailableException in resumeARCoreSession", e);
+      return false;
+    }
+
+    surfaceView.onResume();
+    return true;
+  }
+
+  private Uri createMp4File() {
+    SimpleDateFormat dateFormat = new SimpleDateFormat("yyyyMMdd_HHmmss");
+    String mp4FileName = "arcore-" + dateFormat.format(new Date()) + ".mp4";
+
+    ContentResolver resolver = this.getContentResolver();
+
+    Uri videoCollection = null;
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+      videoCollection = MediaStore.Video.Media.getContentUri(
+          MediaStore.VOLUME_EXTERNAL_PRIMARY);
+    } else {
+      videoCollection = MediaStore.Video.Media.EXTERNAL_CONTENT_URI;
+    }
+
+    // Create a new Media file record.
+    ContentValues newMp4FileDetails = new ContentValues();
+    newMp4FileDetails.put(MediaStore.Video.Media.DISPLAY_NAME, mp4FileName);
+    newMp4FileDetails.put(MediaStore.Video.Media.MIME_TYPE, MP4_VIDEO_MIME_TYPE);
+
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+      // The Relative_Path column is only available since API Level 29.
+      newMp4FileDetails.put(MediaStore.Video.Media.RELATIVE_PATH, Environment.DIRECTORY_MOVIES);
+    } else {
+      // Use the Data column to set path for API Level <= 28.
+      File mp4FileDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_MOVIES);
+      String absoluteMp4FilePath = new File(mp4FileDir, mp4FileName).getAbsolutePath();
+      newMp4FileDetails.put(MediaStore.Video.Media.DATA, absoluteMp4FilePath);
+    }
+
+    Uri newMp4FileUri = resolver.insert(videoCollection, newMp4FileDetails);
+
+    // Ensure that this file exists and can be written.
+    if (newMp4FileUri == null) {
+      Log.e(TAG, String.format("Failed to insert Video entity in MediaStore. API Level = %d", Build.VERSION.SDK_INT));
+      return null;
+    }
+
+    // This call ensures the file exist before we pass it to the ARCore API.
+    if (!testFileWriteAccess(newMp4FileUri)) {
+      return null;
+    }
+
+    Log.d(TAG, String.format("createMp4File = %s, API Level = %d", newMp4FileUri, Build.VERSION.SDK_INT));
+
+    return newMp4FileUri;
+  }
+
+  // Test if the file represented by the content Uri can be open with write access.
+  private boolean testFileWriteAccess(Uri contentUri) {
+    try (java.io.OutputStream mp4File = this.getContentResolver().openOutputStream(contentUri)) {
+      Log.d(TAG, String.format("Success in testFileWriteAccess %s", contentUri.toString()));
+      return true;
+    } catch (java.io.FileNotFoundException e) {
+      Log.e(TAG, String.format("FileNotFoundException in testFileWriteAccess %s", contentUri.toString()), e);
+    } catch (java.io.IOException e) {
+      Log.e(TAG, String.format("IOException in testFileWriteAccess %s", contentUri.toString()), e);
+    }
+
+    return false;
   }
 
   /**
